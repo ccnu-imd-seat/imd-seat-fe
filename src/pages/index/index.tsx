72427ea8c96c1b0388de71515cc5e3df6c0bd470
bingling-sama <bingling_sama@qq.com>
2025-05-20 20:56:09 +0800

    feat: add Prettier and lint-staged for code formatting

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,19 +1,22 @@
-import { View } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import './index.scss'
-import { Button } from "@taroify/core"
+import { View } from '@tarojs/components';
+import { useLoad } from '@tarojs/taro';
+import './index.scss';
+import { Button } from '@taroify/core';
 
-export default function Index () {
+export default function Index() {
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log('Page loaded.');
+  });
 
   return (
-    <View className='index'>
+    <View className="index">
       <View className="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white font-bold text-xl p-6 rounded-xl shadow-2xl mb-6 text-center border-4 border-dashed border-yellow-400">
-        TailwindCSS 测试：渐变背景、白色大号粗体字、圆角、阴影、虚线边框、内边距、居中
+        TailwindCSS
+        测试：渐变背景、白色大号粗体字、圆角、阴影、虚线边框、内边距、居中
       </View>
-      <Button color='primary' onClick={() => console.log("hello")}>Hello world!</Button>
+      <Button color="primary" onClick={() => console.log('hello')}>
+        Hello world!
+      </Button>
     </View>
-  )
+  );
 }
